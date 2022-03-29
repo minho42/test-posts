a01562ec934e6e22ebc02ebd3fef70cd72ee2820
Min ho Kim <minho42@gmail.com>
2022-03-30 00:29:36 +1100

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,78 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { useState, useEffect } from 'react'
+import { createClient } from '@supabase/supabase-js'
 
-const Home: NextPage = () => {
+
+type Post={
+  id: number
+  title: string
+  content: string
+  created_at: string
+}
+
+export default function Posts() {
+  const [posts, setPosts]=useState([])
+  const [title, setTitle]=useState('')
+  const [content, setContent]=useState('')  
+  
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.NEXT_PUBLIC_SUPABASE_API_KEY || '',
+  )
+  
+  const fetchPosts = async () => { 
+    const {data}=await supabase.from('posts').select('*').order('created_at', {ascending: false})
+    console.log(data)
+    setPosts(data)
+  }
+  
+  const createPost = async () => {
+    await supabase.from('posts').insert([{title, content}]).single()
+    fetchPosts()
+  }
+  
+  useEffect(() => {
+    fetchPosts()
+  },[])
+  
+  const handleAdd = (e) => { 
+    e.preventDefault()
+    setTitle('')
+    setContent('')
+    createPost()
+   }
+  
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div className='text-center space-y-3 px-6 max-w-2xl'>
+      <h1 className='text-3xl'>Posts</h1>
+      
+       <form>
+        <div className='flex items-center justify-center space-x-3'>
+          
+          <label htmlFor="title">
+            Title <input id='title' name='title' value={title} placeholder='title' type="text" className='border border-gray-300 px-3 py-2 rounded'
+            onChange={(e)=>setTitle(e.target.value)}
+            />
+          </label>
+          <label htmlFor="content">
+            Content <input id='content' name='content' value={content} placeholder='content' type="text" className='border border-gray-300 px-3 py-2 rounded' 
+            onChange={(e)=>setContent(e.target.value)}
+            />
+          </label>
+          <button 
+          onClick={handleAdd}
+          className='bg-purple-500 hover:bg-purple-600 font-semibold text-white px-6 py-3 rounded-full'>Add</button>
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+      </form>
+      
+      <div className='space-y-3'>
+          {posts.map((post: Post)=>(
+            <div className='bg-purple-100 rounded-lg px-4 py-2 space-y-1'>
+              <div className='text-xl font-semibold'>{post.title}</div>
+              <div className=''>{post.content}</div>
+              <div className='text-gray-500'>{post.created_at.substring(0,10)}</div>
+            </div>
+          ))}  
+      </div>
     </div>
   )
 }
-
-export default Home
